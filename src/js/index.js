f8ef7776abfb9dbd816c72e449dfac9b16996557
Michael Bock <me@thought2.de>
2019-04-22 13:08:20 +0200

    integrate midi library

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,15 @@
-console.log("test")
+import WebMidi from "webmidi";
+
+WebMidi.enable(function(err) {
+  if (err) {
+    console.log("WebMidi could not be enabled.", err);
+    return;
+  }
+  WebMidi.outputs.forEach(function(output) {
+    window.document
+      .getElementById("treble0")
+      .addEventListener("input", function changeControl(e) {
+        output.sendControlChange(14, e.target.value, "all");
+      });
+  });
+});
